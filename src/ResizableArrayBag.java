6883c2aa2e32c20d9510d5be5a759945ccece278
lotusbox <95894554+lotusbox@users.noreply.github.com>
2022-02-20 19:26:47 -0800

    tentative completed ResizableArrayBag

--- src/ResizableArrayBag.java
+++ src/ResizableArrayBag.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array bag implements bag interface to 'inherit' methods that need 
-                                                              //to be customized for a resizable bag   
+                                                              //to be customized for a resizable bag 
 
     private final T[] bag; //final so reference will not change
     private static final int DEFAULT_CAPACITY = 25; //the default size of the array, this is a resizeable array class
@@ -21,6 +21,15 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
         bag = tempBag; //temp bag, our temporary bag, is assigned to bag, our proper bag, becaue is now has an appropriate size
     }
 
+    public ResizableArrayBag(BagInterface<T> copy){ //This "copy constructor" will create a duplicate of a bag so we do not affect
+                                                    //the contents of the original bags
+        numberOfEntries = copy.getCurrentSize(); //duplicates parameter
+        T[] tempBag = copy.toArray(); //duplicates array of objects
+        this.capacity = copy.getCurrentSize(); //duplicates capacity
+        bag = tempBag; //temp bag assigned to proper bag
+    }
+
+
     @Override
     public int getCurrentSize() {
         return numberOfEntries;
@@ -28,25 +37,23 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
 
     @Override
     public boolean isEmpty() {
-        if (numberOfEntries != 0)
-            return false;
-        else
-            return true;
+        return numberOfEntries == 0;
     }
 
     @Override
     public boolean isFull() {
-        return numberOfEntries == capacity - 1;
+        return numberOfEntries == capacity;
     }
 
     @Override
     public boolean add(T newEntry) {
         if (isFull() || newEntry == null){
-            System.out.println("bag is full!");
+            System.out.println("\nWARNING: Could not add \"" + newEntry + "\", bag is full!"); //used for testing
             return false;
         } else{
             bag[numberOfEntries] = newEntry;
             ++numberOfEntries;
+            System.out.println("\"" + newEntry + "\" " + "added at position " + numberOfEntries); //used for testing
             return true;
         }
     }
@@ -54,9 +61,11 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
     @Override
     public T remove() {
         if (isEmpty()){
+            System.out.println("Bag is empty, nothing to remove!"); //used for testing
             return null;
         }else {
             T removed = removeHelper(numberOfEntries - 1);
+            System.out.println(removed + " succesfully removed!"); //used for testing
             return removed;
         }
     }
@@ -65,9 +74,11 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
     public boolean remove(T anEntry) {
         int index = findEntry(anEntry);
         if(index < 0){
+            System.out.println(anEntry + " not found!"); //used for testing
             return false;
         } else {
             removeHelper(index);
+            System.out.println(anEntry + " successfully removed!"); //used for testing
             return true;
         }
     }
@@ -77,6 +88,7 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
         for(int i = 0; i < bag.length; i++){
             bag[i] = null;
         }
+        System.out.println("Clearing contents of bag...\n"); //used for testing
         numberOfEntries = 0;
     }
 
@@ -97,60 +109,87 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
     }
 
     @Override
-    public T[] toArray (T[] a) {
-        return Arrays.copyOf(bag, numberOfEntries);
+    public T[] toArray() {
+        @SuppressWarnings("unchecked")
+        T[] tempbag = (T[])new Object[capacity];
+        for(int i = 0; i < this.numberOfEntries; i++){
+            tempbag[i] = this.bag[i];
+        }
+        return tempbag;
+    }
+
+    public String toString(){
+
+        String output = "\nCapacity: " + this.capacity + "\nEntries: " + this.getCurrentSize() + "\nContents:\n";
+
+        if(this.isEmpty()){
+            output += "<EMPTY>\n";
+        } else for(int i = 0; i < this.numberOfEntries; i++){
+            output += " (" + (i + 1) + ") " + this.bag[i].toString() + "\n";
+        }
+
+        return output;
     }
 
     /****************************************************** 3 METHODS ******************************************************/
 
-    public ResizableArrayBag<T> union(ResizableArrayBag<T> aBag){
-        for(int i = 0; i < aBag.numberOfEntries; i++){
-            this.add(aBag.bag[i]);
-        }
-        return this;
-        /*
-        @SuppressWarnings("unchecked")
-        T[] tempbag = (T[])new Object[this.numberOfEntries + aBag.getCurrentSize];
+    public BagInterface<T> union(BagInterface<T> aBag){ //returns a BagInterface per instructions (means can be arraybag or linked bag)
+        BagInterface<T> tempBag = new ResizableArrayBag<T>(this.numberOfEntries + aBag.getCurrentSize()); //temp bag for output
+        ResizableArrayBag<T> thisClone = new ResizableArrayBag<T>(this); //clone of "this" bag (bag 1)
+        ResizableArrayBag<T> aBagClone = new ResizableArrayBag<T>(aBag); //clone of another bag (bag 2) *cloning is to ensure the original bags are not changed
 
-        for(int i = 0; i < this.numberofEntries; i++){
-            tempbag.add(this[i]);
+        while(!thisClone.isEmpty()){//as long as bag1 is not empty
+            tempBag.add(thisClone.remove()); //remove an item from bag1 add add it to tempbag
         }
-        for(int i = 0; i < aBag.getCurrentSize; i++)
-        {
-            tempbag.add(aBag[i]);
+        while(!aBagClone.isEmpty()){//as long as bag2 is not empty
+            tempBag.add(aBagClone.remove()); //remove an item from bag2 and add it to tempbag
         }
-
-        return tempbag;
-        */ 
+        return tempBag;
     }
 
-    public ResizableArrayBag<T> Intersection(ResizableArrayBag<T> aBag){
-        @SuppressWarnings("unchecked")
-        T[] tempbag = (T[])new Object[DEFAULT_CAPACITY];
+    public BagInterface<T> intersection(BagInterface<T> aBag){
+        //cloning...
+        BagInterface<T> tempBag = new ResizableArrayBag<T>(DEFAULT_CAPACITY);
+        ResizableArrayBag<T> thisClone = new ResizableArrayBag<T>(this);
+        ResizableArrayBag<T> aBagClone = new ResizableArrayBag<T>(aBag);
 
-        for (int i = 0; i < this.numberOfEntries; i++)
-            for (int j = 0; j < aBag.getCurrentSize(); j++){
-                if (this[i] == aBag[j])
-                    tempbag.add(this[i]);
+        while(!thisClone.isEmpty()){ //as long as bag1 is not empty
+            T item = thisClone.remove(); //remove an item from bag1 and hold it
+            if(aBagClone.remove(item)){ //if bag2 contains the removed item, remove it also and return true, true == the same item was removed from both bags
+                tempBag.add(item);//add an item to tempbag IFF an item was removed from bag1 AND bag2
             }
+        }
 
-        return tempbag;
+        //move items from temp bag to a new bag for output
+        //this step is necessary because temp bag's capacity is too big
+        //e.g. tempbag = [a, b, c, null, null, null, ...]
+        //so create an output bag that's only big enough to hold [a, b, c]
+        BagInterface<T> outputBag = new ResizableArrayBag<T>(tempBag.getCurrentSize());
+        while(!tempBag.isEmpty()){
+            outputBag.add(tempBag.remove());
+        }
+
+        return outputBag;
+    }
+
+    public BagInterface<T> difference(ResizableArrayBag<T> aBag){
+        //cloning...
+        BagInterface<T> tempBag = new ResizableArrayBag<T>(DEFAULT_CAPACITY);
+        ResizableArrayBag<T> thisClone = new ResizableArrayBag<T>(this);
+        ResizableArrayBag<T> aBagClone = new ResizableArrayBag<T>(aBag);
+
+        while(!aBagClone.isEmpty()){//while bag2 is not empty
+            thisClone.remove(aBagClone.remove());//remove an item from bag2 and try to remove it from bag1
+        }
         
-    }
+        //move items from tempbag to new bag for output
+        BagInterface<T> outputBag = new ResizableArrayBag<T>(thisClone.getCurrentSize());
+        while(!thisClone.isEmpty()){
+            outputBag.add(thisClone.remove());
+        }
 
-    public ResizableArrayBag<T> Difference(ResizableArrayBag<T> aBag){
-        @SuppressWarnings("unchecked")
-        T[] tempbag = (T[])new Object[DEFAULT_CAPACITY];
+        return outputBag;
 
-        for (int i = 0; i < this.numberOfEntries; i++)
-            for (int j = 0; j < aBag.getCurrentSize(); j++){
-                if (this[i] == aBag[j])
-                    break;
-            }
-            //not sure solution after this, i used break because if they are the same we dont add or need to look further
-            //problem arises because i can figure out how to eliminate the ones that are the same but not how to get the ones that
-            //are not the same to add to tempbag
-        return tempbag;
     }
 
 
@@ -172,7 +211,5 @@ public class ResizableArrayBag<T> implements BagInterface<T>{ //resizable array 
         --numberOfEntries;
         return removedEntry;
     }
-
-    public T[] 
     
 }
